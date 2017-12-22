e57f37e061a81be74737fe966d5be40678cdf4c7
stalfire <engchienhowe@hotmail.com>
2017-12-22 13:49:01 +0800

    create recipe

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,48 @@
+class RecipesController < ApplicationController
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+		@recipe = Recipe.new(params_for_recipe)
+		if @recipe.save
+			redirect_to @recipe
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		@recipe = Recipe.find(params[:id])
+		if @recipe.update(recipe_params)
+			redirect_to @recipe
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@recipe = Recipe.find(params[:id])
+		@recipe.destroy
+		redirect_to root_path
+	end
+
+	def show
+
+	end
+
+	def index
+		@recipes = Recipe.all
+	end
+
+private
+	
+	def params_for_recipe
+		params.require(:recipe).permit(:title, :description)
+		# params.require(:recipe).permit(:title, :description, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
+	end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 require 'bcrypt'
 class User < ApplicationRecord
-	has_many :recipe
+	has_many :recipes
 
 	validates :email, presence: true
   	validates :email, uniqueness: true
