9d53df17a3ee73a6653f22fdd999d53db07b14a7
stalfire <engchienhowe@hotmail.com>
2017-12-22 16:10:53 +0800

    add seach function

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -37,12 +37,18 @@ class RecipesController < ApplicationController
 
 	def index
 		@recipes = Recipe.all
+		filtering_params(params).each do |key, value|
+    	@recipes = @recipes.public_send(key, value) if value.present?
+  		end
 	end
 
 private
 	
 	def params_for_recipe
-		params.require(:recipe).permit(:title, :description,:user_id, ingredients_attributes: [:id, :name, :_destroy], instructions_attributes: [:id, :step, :_destroy])
+		params.require(:recipe).permit(:title, :description,:category,:user_id, ingredients_attributes: [:id, :name, :_destroy], instructions_attributes: [:id, :step, :_destroy])
+	end
+	def filtering_params(params)
+  		params.slice(:category)
 	end
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,6 @@ class Recipe < ApplicationRecord
 	has_many :instructions, dependent: :destroy
 	accepts_nested_attributes_for :ingredients, :reject_if => :all_blank, :allow_destroy => true
 	accepts_nested_attributes_for :instructions, :reject_if => :all_blank, :allow_destroy => true
+
+	scope :category, -> (category) { where category: category}
 end
