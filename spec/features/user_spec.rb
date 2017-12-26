5f8f5f7cea8666d6144166491a051c7fa12918b4
stalfire <engchienhowe@hotmail.com>
2017-12-26 22:37:35 +0800

    create rspec for recipe and user model

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper.rb'
-describe "New", :type => :features do 
-    feature 'User' do 
+RSpec.describe User, :type => :features do 
+    feature 'New' do 
         scenario 'can create a user account' do
             visit '/'
             click_link 'sign up'
@@ -12,24 +12,24 @@ describe "New", :type => :features do
             expect(page).to have_content('rspec_user')
         end
     end
-    feature 'User' do
+    feature 'Current' do
         background do
-            User.create(name: 'rspec_user', email: 'rspec_user@hotmail.com', password: 'rspec_user')
+            User.create(name: 'rspec_user1', email: 'rspec_user1@hotmail.com', password: 'rspec_user1')
         end     
         scenario 'can sign in' do
             visit '/'
             click_link 'sign in'
-            fill_in 'E-mail', with: 'rspec_user@hotmail.com'
-            fill_in 'Password', with: 'rspec_user'
+            fill_in 'E-mail', with: 'rspec_user1@hotmail.com'
+            fill_in 'Password', with: 'rspec_user1'
             click_button 'Login'
 
-            expect(page).to have_content('rspec_user')
+            expect(page).to have_content('rspec_user1')
         end
         scenario 'can edit profile name' do
             visit '/'
             click_link 'sign in'
-            fill_in 'E-mail', with: 'rspec_user@hotmail.com'
-            fill_in 'Password', with: 'rspec_user'
+            fill_in 'E-mail', with: 'rspec_user1@hotmail.com'
+            fill_in 'Password', with: 'rspec_user1'
             click_button 'Login'
             click_link 'Edit Profile'
             fill_in 'Name', with: 'rspec_edit'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  	describe "create" do
+  		context "a new account" do
+  			it "not be save with empty email" do
+  				user = User.create(name: 'rspec_user4', password: 'rspec_user4')
+  				expect(user).to_not be_valid 
+  			end
+  		end
+  	end
 end
