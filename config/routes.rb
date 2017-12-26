e21d3a48358c76c2c018fc2596c65a7be4107981
stalfire <engchienhowe@hotmail.com>
2017-12-26 14:43:37 +0800

    fix like button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	resources :users
 	resources :recipes
-	
+
 	get    '/login' => 'sessions#new'
   	post   '/login' => 'sessions#create'
   	delete '/logout' => 'sessions#destroy'
