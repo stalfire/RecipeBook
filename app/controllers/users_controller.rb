dc26bcc6746bb9073810f38376271bf7b49dd2d6
stalfire <engchienhowe@hotmail.com>
2017-12-27 22:55:19 +0800

    fix user can upload profile image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
 private
 
 	def user_params
-		params.require(:user).permit({avatar: []},:name, :email, :password)
+		params.require(:user).permit(:avatar,:name, :email, :password)
 	end
 
 end
